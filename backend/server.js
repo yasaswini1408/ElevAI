3065f8f29eb66244052ec7d204c9ac68ad432349
yasaswini1408 <yasaswinisamala2005@gmail.com>
2026-07-11 12:12:07 +0530

    deploying this

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true
 }));
 
